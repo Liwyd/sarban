0a04a937a30231e83073367fe321c37967f37be1
Ali Karimi <82864026+Liwyd@users.noreply.github.com>
2024-08-14 22:37:25 +0330

    Delete sarban directory

--- sarban/sub_gen/__init__.py
+++
@@ -1,6 +0,0 @@
-def sub_generator(userToken: str, fullAddress:str, verify: bool = False) -> str:
-
-    ssl = 'https' if verify else "http"
-    string = f"{ssl}://{fullAddress}/sus/{userToken}"
-    
-    return string
